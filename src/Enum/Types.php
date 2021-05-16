f70196d0719be6e625dcab6b5ba17229d9aee79f
Senik Hakobyan <sen.hakobyan@gmail.com>
2021-05-17 00:46:01 +0400

    Implement the Types class as Enum

---
+++ src/Enum/Types.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PhpDto\Enum;
+
+use Enum\Enum;
+
+class Types extends Enum
+{
+	const INT = 'int';
+	const FLOAT = 'float';
+	const BOOL = 'bool';
+	const STRING = 'string';
+	const ARRAY = 'array';
+	const CALLABLE = 'callable';
+}
